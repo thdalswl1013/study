01fbd7913e51d858d8a405cc68f21bb9b69b95f1
thdalswl1013 <thdalswl1013@naver.com>
2023-02-18 12:10:58 +0900

    upload 0218

--- 스터디_self/2606_바이러스.py
+++ 스터디_self/2606_바이러스.py
@@ -1,11 +1,27 @@
 
-computer = int(input()) # 컴퓨터의 수
-pair = int(input()) # 네트워크 상에서 직접 연결되어있는 컴퓨터 쌍의 수
+n = int(input()) # 컴퓨터의 수
+m = int(input()) # 네트워크 상에서 직접 연결되어있는 컴퓨터 쌍의 수
 
-arr=[]
+#arr=[]
+graph = [[]*n for _ in range(n+1)]
 
-for i in range(pair):
+for i in range(m):
     x, y= map(int, input().split())
-    arr.append([x,y])
+    #arr.append([x,y])
+    graph[x].append(y)
+    graph[y].append(x)
 
-print(arr) # [[1, 2], [2, 3], [1, 5], [5, 2], [5, 6], [4, 7]]
+cnt=0
+visited=[0]*(n+1)
+
+def dfs(start): # 그래프 탐색 -> dfs 
+    global cnt
+    visited[start]=1
+
+    for i in graph[start]:
+        if visited[i]==0:
+            dfs(i)
+            cnt+=1
+            
+dfs(1)
+print(cnt)
