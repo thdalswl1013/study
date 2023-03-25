6472510d69984e27cc68fc9e97328f1d450e029e
thdalswl1013 <thdalswl1013@naver.com>
2023-03-25 11:28:53 +0900

    upload 0325

---
+++ 스터디_self/10866_덱.py
@@ -0,0 +1,19 @@
+n=int(input())
+arr=[]
+for i in range(n):
+    arr.append(input().split())
+
+for i in range(n):
+    if arr[i][0]=='push_back' or arr[i][0]=='push_front':
+        arr[i][1]=int(arr[i][1])
+
+"""
+push_front X: 정수 X를 덱의 앞에 넣는다.
+push_back X: 정수 X를 덱의 뒤에 넣는다.
+pop_front: 덱의 가장 앞에 있는 수를 빼고, 그 수를 출력한다. 만약, 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+pop_back: 덱의 가장 뒤에 있는 수를 빼고, 그 수를 출력한다. 만약, 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+size: 덱에 들어있는 정수의 개수를 출력한다.
+empty: 덱이 비어있으면 1을, 아니면 0을 출력한다.
+front: 덱의 가장 앞에 있는 정수를 출력한다. 만약 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+back: 덱의 가장 뒤에 있는 정수를 출력한다. 만약 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.        
+"""
