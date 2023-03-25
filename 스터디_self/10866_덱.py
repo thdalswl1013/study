67632e7b1bda3341bc52fde9cd28aba250dc6301
thdalswl1013 <thdalswl1013@naver.com>
2023-03-25 14:04:23 +0900

    upload 0325

--- 스터디_self/10866_덱.py
+++ 스터디_self/10866_덱.py
@@ -1,19 +1,63 @@
-n=int(input())
-arr=[]
-for i in range(n):
-    arr.append(input().split())
+from collections import deque # 덱 = 양방향 큐 
+import sys
+
+d = deque() # 튜플
+n = int(input())
 
 for i in range(n):
-    if arr[i][0]=='push_back' or arr[i][0]=='push_front':
-        arr[i][1]=int(arr[i][1])
+    sen = sys.stdin.readline().split()
+    # print(sen)
 
-"""
-push_front X: 정수 X를 덱의 앞에 넣는다.
-push_back X: 정수 X를 덱의 뒤에 넣는다.
-pop_front: 덱의 가장 앞에 있는 수를 빼고, 그 수를 출력한다. 만약, 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
-pop_back: 덱의 가장 뒤에 있는 수를 빼고, 그 수를 출력한다. 만약, 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
-size: 덱에 들어있는 정수의 개수를 출력한다.
-empty: 덱이 비어있으면 1을, 아니면 0을 출력한다.
-front: 덱의 가장 앞에 있는 정수를 출력한다. 만약 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
-back: 덱의 가장 뒤에 있는 정수를 출력한다. 만약 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.        
-"""
+    if sen[0] == "push_front": # 정수를 덱의 앞에 넣는다. -> appendleft() 함수를 이용.
+        d.appendleft(sen[1])
+
+
+    elif sen[0] == "push_back": # 정수를 덱의 뒤에 넣는다. 
+        d.append(sen[1])
+
+
+    elif sen[0] == "pop_front": # 덱의 가장 앞에 있는 수를 빼고, 그 수를 출력한다. -> popleft() 함수를 이용.
+                                # 만약, 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+        if d:
+            print(d[0])
+            d.popleft()
+
+        else:
+            print(-1)
+
+
+    elif sen[0] == "pop_back": # 덱의 가장 뒤에 있는 수를 빼고, 그 수를 출력한다.
+                               # 만약, 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+        if d:
+            print(d[len(d) - 1])    
+            d.pop()
+        else:
+            print(-1)
+
+
+    elif sen[0] == "size": # 덱에 들어있는 정수의 개수를 출력한다.
+        print(len(d))
+
+
+    elif sen[0] == "empty": # 덱이 비어있으면 1을, 아니면 0을 출력한다.
+        if d:
+            print(0)
+        else:
+            print(1)
+
+
+    elif sen[0] == "front": # 덱의 가장 앞에 있는 정수를 출력한다.
+                            # 만약 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+
+        if d: 
+            print(d[0])
+        else:
+            print(-1)
+
+
+    elif sen[0] == "back": # 덱의 가장 뒤에 있는 정수를 출력한다. 
+                           # 만약 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.        
+        if d:
+            print(d[len(d) - 1])
+        else:
+            print("-1")
