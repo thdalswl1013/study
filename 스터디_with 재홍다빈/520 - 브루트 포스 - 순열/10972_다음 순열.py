27d8dfc707db0a49a03b2ec586319816293af897
thdalswl1013 <thdalswl1013@naver.com>
2023-05-11 09:02:22 +0900

    upload 0511

--- 스터디_with 재홍다빈/520 - 브루트 포스 - 순열/10972_다음 순열.py
+++ 스터디_with 재홍다빈/520 - 브루트 포스 - 순열/10972_다음 순열.py
@@ -1,3 +1,19 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+for i in range(n-1, 0, -1): # 맨 뒤 값부터 시작
+    if arr[i-1] < arr[i]:
+        for j in range(n-1, 0, -1): # 다시 맨 뒤 값부터 큰 값 찾기
+            if arr[i-1] < arr[j]:
+                arr[i-1], arr[j] = arr[j], arr[i-1] # 값 서로 바꾸기
+                arr = arr[:i] + sorted(arr[i:])
+                
+                for i in arr:
+                    print(i, end=' ')
+                exit()
+print(-1)
+
+"""
 import itertools
 
 n=int(input())
@@ -24,3 +40,4 @@ for i in range(len(list(nPr))):
 
 for i in range(len(result)):
     print(result[i],end=' ')
+"""
