e82a6ce3f1f179755ed0bf9c4cf5ad01efeb4cfb
thdalswl1013 <thdalswl1013@naver.com>
2023-07-06 16:55:39 +0900

    upload 0706

--- 스터디_with 재홍다빈/520 - 브루트 포스 - 순열/10973_이전 순열.py
+++ 스터디_with 재홍다빈/520 - 브루트 포스 - 순열/10973_이전 순열.py
@@ -10,3 +10,16 @@ for i in range(n):
 if n_arr==arr:
     print(-1)
 """
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+for i in range(n - 1, 0, -1):
+    if arr[i - 1] > arr[i]:
+        for j in range(n - 1, 0, -1):
+            if arr[i - 1] > arr[j]:
+                arr[i - 1], arr[j] = arr[j], arr[i - 1]
+                arr = arr[:i] + sorted(arr[i:],reverse=True)
+                print(*arr)
+                exit()
+print(-1)
