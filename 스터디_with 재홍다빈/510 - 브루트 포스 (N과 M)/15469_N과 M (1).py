010b92f8ae4759b252e195276563963c68b351eb
thdalswl1013 <thdalswl1013@naver.com>
2023-07-15 13:52:42 +0900

    upload 0715

--- 스터디_with 재홍다빈/510 - 브루트 포스 (N과 M)/15469_N과 M (1).py
+++ 스터디_with 재홍다빈/510 - 브루트 포스 (N과 M)/15469_N과 M (1).py
@@ -1,4 +1,18 @@
 # "1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열" 조건을 만족하는
 # 길이가 M인 수열을 모두 구하시오
 
-n, m = map(int, input().split()) # 3, 1
+n, m = list(map(int, input().split())) # 3, 1 
+s = []
+
+def dfs():
+    if len(s) == m:
+        print(' '.join(map(str, s)))
+        return
+    
+    for i in range(1, n + 1):
+        if i not in s:
+            s.append(i)
+            dfs()
+            s.pop()
+            
+dfs()
